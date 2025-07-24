f0d5fff26c0dc4ede5691299957e1ea45c01d3d6
mlmg08 <lorens.mendez91@gmail.com>
2025-07-24 19:47:04 -0300

    Cambio en prueba Registro cliente

--- cypress/e2e/RegistroClienteMM.cy.js
+++ cypress/e2e/RegistroClienteMM.cy.js
@@ -13,8 +13,8 @@ describe('Prueba de Registro de Cliente', () => {
 it('Registro con datos validos', () => { 
     cy.get('.grid > :nth-child(1) > .tap-highlight-transparent > .inline-flex').type('Lorenas Movies')
     cy.get('.grid > :nth-child(2) > .relative > .inline-flex').type('27956725687')
-    cy.get('[data-cy="select-provincia"]').select('Ciudad Autónoma de Buenos Aires')
-    cy.get('[data-cy="select-localidad"]').select('Caballito')
+    cy.get('[data-cy="select-provincia"]').type('Ciudad Autónoma de Buenos Aires' + '{enter}')
+    cy.get('[data-cy="select-localidad"]').type('Caballito' + '{enter}')
     cy.get('.grid > :nth-child(5) > .relative > .inline-flex').type('Av. San Martin 1567')
     cy.get(':nth-child(6) > .relative > .inline-flex').type('1126405620')
     cy.get(':nth-child(7) > .relative > .inline-flex').type('lorens_mendez@hotmail.com')
@@ -25,11 +25,11 @@ it('Registro con datos validos', () => {
 
 })
 
-it.skip('Registro con datos invalidos', () => { 
+it('Registro con datos invalidos', () => { 
     cy.get('.grid > :nth-child(1) > .tap-highlight-transparent > .inline-flex').type('hiohíhihuhk')
     cy.get('.grid > :nth-child(2) > .relative > .inline-flex').type('455544555555')
-    cy.get('[data-cy="select-provincia"]').select('Corrientes')
-    cy.get('[data-cy="select-localidad"]').select('Alvear')
+    cy.get('[data-cy="select-provincia"]').type('Corrientes' + '{enter}')
+    cy.get('[data-cy="select-localidad"]').type('Alvear' + '{enter}')
     cy.get('.grid > :nth-child(5) > .relative > .inline-flex').type('jgjgukñkjnñkjpu')
     cy.get(':nth-child(6) > .relative > .inline-flex').type('5555555546')
     cy.get(':nth-child(7) > .relative > .inline-flex').type('lore@gmail.com')
